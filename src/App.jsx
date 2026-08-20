33c7e8b547e2c762420025aa01f5c5027697e7ae
greeme99 <greeme99@gmail.com>
2026-08-20 18:25:44 +0900

    Configure GitHub Pages deployment (gh-pages, HashRouter, base URL)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AnalysisProvider } from './contexts/AnalysisContext';
 import AppLayout from './components/layout/AppLayout';
 
